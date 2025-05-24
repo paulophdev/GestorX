bf045aca51da400879556f4471fefd6455c92513
Paulo H N Matos <paulophdev@gmail.com>
2025-05-24 12:13:53 -0300

    Teste deploy

--- docker/start.sh
+++ docker/start.sh
@@ -1,3 +1,15 @@
-#!/bin/sh
+#!/bin/bash
 
-exec php-fpm 
+# Verifica se a pasta vendor existe; se não, instala as dependências
+if [ ! -d "/var/www/vendor" ]; then
+    echo "Installing Composer dependencies..."
+    composer install --no-dev --optimize-autoloader
+fi
+
+# Executa os comandos Artisan, se necessário
+php artisan key:generate
+php artisan config:cache
+php artisan migrate --force
+
+# Inicia o servidor
+php artisan serve --host=0.0.0.0 --port=10000
